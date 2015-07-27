60ab70bc28fcaacb91cfe36ff2479c6a2783951d
Jeffrey Ruane <ruane.jb@gmail.com>
2015-07-27 11:40:07 -0600

    offset working

---
+++ test/offset_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require '../lib/offset'
+
+class DateOffsetTest < Minitest::Test
+  def test_it_exists
+    offset = DateOffset.new
+    assert offset
+  end
+
+  def test_it_can_get_current_date_in_UTC
+    skip
+    current_date = DateOffset.new
+    assert_equal current_date, Time.new.strftime("%d%m%y").to_i
+  end
+
+  def test_it_can_square_the_date
+    current_date = DateOffset.new
+    date_square = current_date.date_square
+    expected = Time.new.strftime("%d%m%y").to_i ** 2
+    assert_equal date_square, expected
+  end
+
+  def test_it_returns_4_characters
+    date = DateOffset.new.offset
+    assert_equal date.length, 4
+  end
+end
