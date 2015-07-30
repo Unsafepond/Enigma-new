44e1e2c75358dd3176f0a611d345fcebaa1296ee
Jeffrey Ruane <ruane.jb@gmail.com>
2015-07-30 08:44:19 -0600

    runner will call crack, but crack doesn't work

--- lib/runner.rb
+++ lib/runner.rb
@@ -2,6 +2,7 @@ require './lib/keygen'
 require './lib/offset'
 require './lib/encryptor'
 require './lib/decryptor'
+require './lib/crack'
 
 class Runner
 
@@ -12,7 +13,10 @@ class Runner
       output_text = FileIO.new.write_file(decrypted.decrypt)
       puts "Created #{ARGV[1]} with the key #{options[:key]} and the date #{options[:date]}"
     elsif options[:date]
-      CrackRunner.new(options)
+      offset = DateOffset.new(options[:date].to_i).offset
+      cracked = Crack.new(options[:infile], offset)
+      output_text = FileIO.new.write_file(cracked.decrypt_crack)
+      puts "Created #{ARGV[1]} with the key #{} and the date #{options[:date]}"
     else
       encrypted = Encryptor.new(options[:infile], key = Keygen.new.generate,
         offset = DateOffset.new.offset)
